753777d226cfd7f5bce00d6e8ca54eb880c5cf0f
G00403720 <G00403720@atu.ie>
2022-11-28 10:44:16 +0000

    TestPerson

--- src/main/java/TestPerson.java
+++ src/main/java/TestPerson.java
@@ -3,10 +3,11 @@ public class TestPerson {
         Person chi = new Person();
         chi.setName("Chi");
         chi.setAddress("Ballybane");
-        chi.setTelephone("1358");
+        chi.setTelephone("0874721950");
 
-        System.out.println("Chi details: " + chi.getName());
-        System.out.println("Chi details: " + chi.getAddress());
-        System.out.println("Chi details: " + chi.getTelephone());
+        System.out.println("Chi details: " + chi.toString());
+
+        Customer myCust = new Customer("Chi", "Ballybane", "0874721950", "0871358470", true );
+        System.out.println("Details " + myCust.toString());
     }
 }
