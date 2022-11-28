a42900246b1eebf26c029f3039d3efbd0454916b
G00403720 <G00403720@atu.ie>
2022-11-28 09:45:26 +0000

    TestPerson

--- src/main/java/TestPerson.java
+++ src/main/java/TestPerson.java
@@ -1,12 +1,1 @@
-public class TestPerson {
-    public static void main(String[] args) {
-        Person chi = new Person();
-        chi.setName("Chi");
-        chi.setAddress("Ballybane");
-        chi.setTelephone("1358");
 
-        System.out.println("Chi details: " + chi.getName());
-        System.out.println("Chi details: " + chi.getAddress());
-        System.out.println("Chi details: " + chi.getTelephone());
-    }
-}
